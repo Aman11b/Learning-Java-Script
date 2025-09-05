8a6f17b81a04551a1aaef54fc699ee829aed3997
Aman Singh <amansingh1100b@gmail.com>
2025-09-06 02:30:54 +0530

    console

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -1,2 +1,16 @@
+// ======Console=======
+
 // alert("hello js");
 console.log("hello JS");
+// console > object.methods
+console.log("string");
+console.warn("waring!");
+console.table({ name: "hi", age: 20 });
+// clg =>console.log
+// log => console.log
+
+// ======Variables========
+// var let const
+let n;
+n = "Am name";
+console.log(n);
