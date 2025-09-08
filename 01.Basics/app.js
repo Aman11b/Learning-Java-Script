ff06daf0bbd99c3d4e3f655bf8db144731670552
Aman Singh <amansingh1100b@gmail.com>
2025-09-08 23:16:28 +0530

    Strings

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -60,3 +60,41 @@ console.log(isNaN);
 // !== strict non equality (type and value)
 // == loose equality (value)
 // != loose equality (value)
+
+// string "" '' ``
+let na = "Aman";
+let ls = "singh";
+
+console.log("string", na);
+
+console.log(`My name is ${na}`);
+console.log(`Full name ${na + ls}`);
+
+// concatination
+let ful = na.concat(ls);
+console.log(`Concat=> ${ful}`);
+
+// Append
+let ful1 = ful + " doing something";
+console.log(`Append=> ${ful1}`);
+
+// length
+console.log(`Length=> ${ful1.length}`);
+
+// cases
+console.log(`Lower case=> ${ful1.toLowerCase()}`);
+console.log(`Upper case=> ${ful1.toUpperCase()}`);
+
+// slice
+console.log(`Slice=> ${ful1.slice(2, 5)}`);
+
+// split and join
+console.log(`Split=> ${ful1.split("")}`);
+console.log(`Join=> ${ful1.split("").join("-")}`);
+
+// include
+console.log(`includes=> ${ful1.includes("s")}`);
+
+// Trim
+var nen = "    adsda  asdasd     ";
+console.log(`Trim=> ${nen.trim()}`);
