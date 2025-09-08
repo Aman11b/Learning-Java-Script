0482f1f586b003647e4a01204756b2b369ee8ed5
Aman Singh <amansingh1100b@gmail.com>
2025-09-08 16:18:16 +0530

    variables,numbers,boolean,comparitive operator

--- 01.Basics/app.js
+++ 01.Basics/app.js
@@ -14,3 +14,49 @@ console.table({ name: "hi", age: 20 });
 let n;
 n = "Am name";
 console.log(n);
+
+//  let const var
+//-> let var value can be changed
+// -> const you cant change again
+
+//=====primitive types====
+//1. Numbers
+let num = 10;
+let num1 = 10.1;
+console.log(num, typeof num);
+console.log(num1, typeof num1);
+
+console.log("2+2 =", 2 + 2);
+console.log("2-2 =", 2 - 2);
+console.log("2*2 =", 2 * 2);
+console.log("2/2 =", 2 / 2);
+console.log("2%2 =", 2 % 2);
+console.log("2^2 =", 2 ** 2);
+
+let count = 10;
+console.log("counter =", count);
+count++;
+console.log("counter++ =", count);
+count--;
+console.log("counter-- =", count);
+
+//2.Boolean
+let isTrue = true;
+console.log(isTrue);
+let isNaN = NaN;
+console.log(isNaN);
+
+// falsy value -> false, null, undefined, 0, -0, NaN, "", ''. ``, (empty quotes)
+
+// null set by programmer
+// undefined set by JScompiler
+
+// comparison operator
+// -> relational operator
+// > ,<,>=,<=
+
+// -> equality operator
+// === strict equality (type and value)
+// !== strict non equality (type and value)
+// == loose equality (value)
+// != loose equality (value)
