4af921ef61119f88e63122a4377a3b904993bf50
Aman Singh <amansingh1100b@gmail.com>
2025-09-15 22:48:46 +0530

    arrays

--- 02.Control Flow/main.js
+++ 02.Control Flow/main.js
@@ -62,3 +62,61 @@ console.log("logical operator");
 console.log(true && true);
 console.log(false || true);
 console.log(!true);
+
+// Arrays -> 0 index based
+
+// empty array
+const empty = [];
+console.log(empty);
+
+const num = [1, 2, 3, 4, 5];
+const nums = [3, 4, 5, 6];
+console.log(num);
+
+const strings = ["m", "a", "d", "r"];
+console.log(strings);
+
+// accessing items
+console.log(num[4]);
+
+// nested array
+const nested = [1, 2, 3, [2, 3, 4], 5];
+console.log(nested[3]);
+
+// array methods
+// push
+num.push(8);
+console.log(num);
+
+num.pop();
+console.log(num);
+
+// shift-> delete from start
+console.log(num);
+num.shift();
+console.log(num);
+
+// unshift-> add at start
+num.unshift(1);
+console.log(num);
+
+//concat
+const conc = num.concat(nums);
+console.log(conc);
+
+// includes
+console.log(num.includes(1));
+
+// join
+console.log(num.join("-"));
+
+// revert
+console.log(num.reverse());
+num.reverse();
+
+// slice
+console.log(num.slice(0, 2));
+
+// sort
+num.reverse();
+console.log(num.sort());
