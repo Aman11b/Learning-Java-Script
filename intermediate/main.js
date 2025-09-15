b1b4db3f7282615dbed3f157bfd3ff2fa9e22992
Aman Singh <amansingh1100b@gmail.com>
2025-09-16 03:50:49 +0530

    rest function

--- intermediate/main.js
+++ intermediate/main.js
@@ -41,3 +41,13 @@ const obj1 = { x: 1, y: 2 };
 const obj2 = { z: 3 };
 const obj3 = { ...obj1, ...obj2 };
 console.log(obj3);
+
+// rest operator
+console.log("rest operator->>>");
+// accept indefinite number of arguments ,as array provide a way to represent variadic function
+
+function user(x, ...data) {
+  console.log(x);
+  console.log(data);
+}
+user("1", 2, 3, 4, 5, 6, 7);
