ed5df6434fc468c74dcd39ab5766e06666b23930
Aman Singh <amansingh1100b@gmail.com>
2025-09-16 03:44:25 +0530

    spread operator

--- intermediate/main.js
+++ intermediate/main.js
@@ -14,3 +14,30 @@ const double = (n) => n * 2;
 console.log(double(4));
 
 // spread operator
+
+console.log("spread operator->>>");
+// function
+function giveMe4(a, b, c, d) {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+  console.log("d", d);
+}
+const colors = ["r", "b", "g", "y"];
+giveMe4(1, 2, 3, 4);
+giveMe4(colors);
+
+giveMe4(...colors);
+// array expanded
+
+// arrays
+const numbs = [1, 2, 3, 4, 5, 6];
+const num1 = [4, 5, 6, 7];
+const con1 = [...numbs, ...num1];
+console.log(con1);
+
+// object
+const obj1 = { x: 1, y: 2 };
+const obj2 = { z: 3 };
+const obj3 = { ...obj1, ...obj2 };
+console.log(obj3);
